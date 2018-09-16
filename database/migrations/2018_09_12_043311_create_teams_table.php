c7badcff97aeefd00a43341fa48eeb221c7ae45e
alfana21 <you@example.com>
2018-09-16 08:45:13 -0500

    Carga

---
+++ database/migrations/2018_09_12_043311_create_teams_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            //--------------foreen key-------------------------------------
+            $table->unsignedInteger('league_id');
+            $table->foreign('league_id')->references('id')->on('leagues');
+            //-------------------------------------------------------------
+            $table->string('name');
+            $table->string('country');
+            $table->string('web');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
---
+++ database/migrations/2018_09_12_044005_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            //--------------foreen key-------------------------------------
+            $table->unsignedInteger('team_id');
+            $table->foreign('team_id')->references('id')->on('teams');
+            //-------------------------------------------------------------
+            $table->string('name');
+            $table->string('nationality');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
